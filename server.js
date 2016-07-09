0e1e4d93968830279333eeff9d515fcbe82283f1
John Doe <andrey.roba@gmail.com>
2016-07-09 16:48:18 +0300

    Add logging and 404 route

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 // setup modules
 var express         = require('express');
+var logger          = require('morgan');
 var bodyParser      = require('body-parser');
 var mongoose        = require('mongoose');
 // setup express
 var app             = express();
 
+// use logger
+app.use(logger('dev'));
 // use middleware to parse application/json
 app.use(bodyParser.json());
 // use custom router
 app.use('/', require('./app/route/note-rt'));
+// if no route matched so far reply with 404
+app.use(function (req, res, next) {
+    var err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
 
 // start mongo
 mongoose.connect('mongodb://localhost/notes', function (err) {
