b1802a0bf1d53b256af8e4e34f63d903af223e9e
John Doe <andrey.roba@gmail.com>
2016-07-09 22:06:58 +0300

    Add Hydrogen SPA template (Angular-ready)

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+window.notesUI = (function () {
+    'use strict';
+
+    // TODO here we will adapt hydrogen's original main.js for use in our Angular views
+
+    return {
+    }
+}());
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express         = require('express');
 var logger          = require('morgan');
 var bodyParser      = require('body-parser');
 var mongoose        = require('mongoose');
+var favicon         = require('serve-favicon');
 // setup express
 var app             = express();
 
@@ -12,6 +13,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 // use custom router
 app.use('/', require('./app/route/note-rt'));
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+// set express to serve libraries from bower standard directory
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 // if no route matched so far reply with 404
 app.use(function (req, res, next) {
     var err = new Error('Not found');
